a94af8235d100f5c7aaf123b365931575e0d9b6d
Juan Antonio <132228341+JuanSho780@users.noreply.github.com>
2025-07-15 15:36:22 -0500

    Hide pass

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -3,7 +3,7 @@ import { Sequelize } from 'sequelize';
 
 const hostname = "localhost";
 const username = "postgres";
-const password = "Juanzafi1$";
+const password = "password";
 const port = 5432;
 const dialect = "postgres";
 const database = "prograWebProyDB";
